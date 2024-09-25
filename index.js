0648e9ad5d37ba7a7cb242b353ea9af6c10f8ccd
jsn-sil <joelsonid@gmail.com>
2024-09-25 11:54:16 -0300

    apredemos if e else

---
+++ index.js
@@ -0,0 +1,33 @@
+let nome;
+
+if(nome){
+    document.write('seja bem-vindo '+nome+ '!');
+} else {
+    document.write('seja bem-vindo ');
+    
+}
+
+//let numero = prompt ('Digite um numero');
+
+//if(numero % 2 == 0){
+// console.log ('E par');  
+//} else {
+//   console.log('É impar');
+// }
+
+document.write('<br><br>');
+
+let idade = prompt ('Qual é a idade?');
+let habilitação = prompt ('Possui habilitação');
+
+if(idade >= 18 && habilitado == 'sim'){
+document.write ('Pode Dirigir');
+}
+
+else {
+    document.write ('Não pode Dirigir');
+}
+
+
+// ()  condição
+// {}  bloco de condição
